67c25160707a9d95019aedb43918b180debcb8f9
Sebastian <sebastian-010198@hotmail.com>
2020-06-08 20:30:17 -0500

    update pdf report

--- public/js/diagnotico.js
+++ public/js/diagnotico.js
@@ -184,8 +184,29 @@ function descargar() {
     })
         .then(res => res.blob())
         .then(blob => {
-            var file = window.URL.createObjectURL(blob);
-             window.location.assign(file);
+            // It is necessary to create a new blob object with mime-type explicitly set
+            // otherwise only Chrome works like it should
+            var newBlob = new Blob([blob], { type: "application/pdf" });
+
+            // IE doesn't allow using a blob object directly as link href
+            // instead it is necessary to use msSaveOrOpenBlob
+            if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+                window.navigator.msSaveOrOpenBlob(newBlob);
+                return;
+            }
+
+            // For other browsers: 
+            // Create a link pointing to the ObjectURL containing the blob.
+            const data = window.URL.createObjectURL(newBlob);
+            // var link = document.createElement('a');
+            // link.href = data;
+            // link.download = "file.pdf";
+            // link.click();
+            setTimeout(function () {
+                // For Firefox it is necessary to delay revoking the ObjectURL
+                window.open(data);
+                window.URL.revokeObjectURL(data);
+            }, 100);
         });
 }
 
